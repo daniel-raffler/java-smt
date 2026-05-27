4bf2e7fe3f1c6427bff5c1bf7294b85ee1dab311
Daniel Raffler <daniel-raffler@gmx.de>
2026-05-27 15:03:01 +0200

    ..also CVC4

--- doc/Example-Maven-Project/src/main/java/org/sosy_lab/java_smt_example/SolverOverviewTable.java
+++ doc/Example-Maven-Project/src/main/java/org/sosy_lab/java_smt_example/SolverOverviewTable.java
@@ -30,7 +30,7 @@ public class SolverOverviewTable {
         new org.sosy_lab.java_smt.example.SolverOverviewTable();
     final List<SolverInfo> infos = new ArrayList<>();
     for (Solvers s : Solvers.values()) {
-      if (s != Solvers.MATHSAT5) {
+      if (s != Solvers.MATHSAT5 &&  s != Solvers.CVC4) {
         infos.add(infoProvider.getSolverInformation(s));
       }
     }
--- doc/Example-Maven-Project/src/test/java/org/sosy_lab/java_smt_example/SudokuTest.java
+++ doc/Example-Maven-Project/src/test/java/org/sosy_lab/java_smt_example/SudokuTest.java
@@ -145,7 +145,7 @@ public class SudokuTest {
   public void checkSudoku()
       throws InvalidConfigurationException, InterruptedException, SolverException {
     assumeTrue(isOperatingSystemSupported(solver));
-    if (s != Solvers.MATHSAT5) {
+    if (s != Solvers.MATHSAT5 && s != Solvers.CVC4) {
       logger.log(Level.INFO, "Executing " + solver + "...");
 
       context = SolverContextFactory.createSolverContext(config, logger, notifier, solver);
