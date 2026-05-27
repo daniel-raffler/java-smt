aab17137c4b25c9814266820bf827c589e7caa32
Daniel Raffler <daniel-raffler@gmx.de>
2026-05-27 15:06:30 +0200

    ..and Z3 4.5

--- doc/Example-Maven-Project/src/main/java/org/sosy_lab/java_smt_example/SolverOverviewTable.java
+++ doc/Example-Maven-Project/src/main/java/org/sosy_lab/java_smt_example/SolverOverviewTable.java
@@ -30,7 +30,8 @@ public class SolverOverviewTable {
         new org.sosy_lab.java_smt.example.SolverOverviewTable();
     final List<SolverInfo> infos = new ArrayList<>();
     for (Solvers s : Solvers.values()) {
-      if (s != Solvers.MATHSAT5 &&  s != Solvers.CVC4) {
+      if (s != Solvers.MATHSAT5 &&  s != Solvers.CVC4 && s != Solvers.Z3_WITH_INTERPOLATION) {
+        System.out.println(s);
         infos.add(infoProvider.getSolverInformation(s));
       }
     }
