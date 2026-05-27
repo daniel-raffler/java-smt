db288a061cd6b872d1467c475836fece5214172e
Daniel Raffler <daniel-raffler@gmx.de>
2026-05-27 20:53:39 +0200

    Print linking errors for debugging

--- doc/Example-Ivy-Project/src/main/org/sosy_lab/java_smt_example/SolverOverviewTable.java
+++ doc/Example-Ivy-Project/src/main/org/sosy_lab/java_smt_example/SolverOverviewTable.java
@@ -2,45 +2,33 @@
 // an API wrapper for a collection of SMT solvers:
 // https://github.com/sosy-lab/java-smt
 //
-// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
 //
 // SPDX-License-Identifier: Apache-2.0
 
 package org.sosy_lab.java_smt_example;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import org.sosy_lab.common.ShutdownNotifier;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.java_smt.SolverContextFactory;
 import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
-import org.sosy_lab.java_smt.api.SolverException;
-import org.sosy_lab.java_smt.example.SolverOverviewTable.RowBuilder;
-import org.sosy_lab.java_smt.example.SolverOverviewTable.SolverInfo;
+import org.sosy_lab.java_smt.api.SolverContext;
 
-/**
- * This program takes all installed solvers and checks them for version, theories and features and
- * prints them to StdOut in a nice table.
- *
- * <p>This is just a copy of org.sosy_lab.java_smt.example.SolverOverviewTable
- */
 public class SolverOverviewTable {
+  public static void main(String[] args) {
+    Configuration config = Configuration.defaultConfiguration();
+    LogManager logger = LogManager.createNullLogManager();
+    ShutdownNotifier notifier = ShutdownNotifier.createDummy();
 
-  public static void main(String[] args) throws SolverException, InterruptedException {
-
-    final org.sosy_lab.java_smt.example.SolverOverviewTable infoProvider =
-        new org.sosy_lab.java_smt.example.SolverOverviewTable();
-    final List<SolverInfo> infos = new ArrayList<>();
-    for (Solvers s : Solvers.values()) {
-      infos.add(infoProvider.getSolverInformation(s));
+    for (Solvers solver : Solvers.values()) {
+      try (SolverContext context =
+               SolverContextFactory.createSolverContext(config, logger, notifier, solver)) {
+        System.out.println(solver + ", " + context.getVersion());
+      } catch (InvalidConfigurationException e) {
+        System.out.println(solver + " not available: " + e.getMessage());
+      }
     }
-
-    infos.sort(Comparator.comparing(SolverInfo::getName)); // alphabetical ordering
-
-    RowBuilder rowBuilder = new RowBuilder();
-    for (SolverInfo info : infos) {
-      rowBuilder.addSolver(info);
-    }
-    System.out.println(rowBuilder);
   }
-
-  private SolverOverviewTable() {}
 }
